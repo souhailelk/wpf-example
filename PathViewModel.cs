c971c02ef366d979a5f0b9aae8ed04631b5f71bd
souhailelk <elkaissi.souhail.me@gmail.com>
2023-04-26 08:28:56 +0100

    Add Wpf example

---
+++ PathViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace wpfApp
+{
+
+    internal class PathViewModel : ObservableObject
+    {
+        public PathViewModel()
+        {
+            ChangePathCommand = new RelayCommand(ChangePathContent);
+            path = "noting";
+        }
+        private string path;
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                if (path != value)
+                {
+                    path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+        public RelayCommand ChangePathCommand { get; }
+
+        private void ChangePathContent() => Path = $"{Guid.NewGuid()}";
+
+    }
+}
